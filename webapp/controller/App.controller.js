9f1e9693809cff1f5589dc954d11dbc474e41dba
Baran <baran.solen@gmail.com>
2024-10-03 16:24:10 +0200

    v0.4 - Design impl

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,15 +4,27 @@ sap.ui.define([
 ], (Controller, MessageToast) => {
 	"use strict";
 
-	return Controller.extend("renumactionlistui.controller.App", {
-		onShowHello() {
-			// read msg from i18n model
-			const oBundle = this.getView().getModel("i18n").getResourceBundle();
-			const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+	return Controller.extend("renumactionlistui.controller.App", {		
+		handleTypeMissmatch: function(oEvent) {
+			console.log("Triggered mismatch");
+			var oFile = oEvent.getParameter("files")[0];  // Get the first uploaded file
+			var sFileName = oFile.name;
+		
+			if (!sFileName.endsWith(".csv")) {
+				// File extension is valid, proceed with upload or further processing
+				sap.m.MessageToast.show("Please upload a valid CSV file.");
+			} 		
+		},
 
-			// show message
-			MessageToast.show(sMsg);
+		onRenumberPress: function() {
+			var oFileUploader = this.byId("fileUploader");
+			oFileUploader.checkFileReadable().then(function() {
+				oFileUploader.upload();
+			}, function(error) {
+				MessageToast.show("Error: The file cannot be read.");
+			}).then(function() {
+				oFileUploader.clear();
+			});
 		}
 	});
 });
